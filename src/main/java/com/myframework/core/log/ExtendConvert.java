0bb6f195f31a5c2dee0ebef9d7378a45a16b69d7
赵伟 <zhaowei_ha@126.com>
2017-07-24 20:19:37 +0800

    日志打印出 用户id

---
+++ src/main/java/com/myframework/core/log/ExtendConvert.java
@@ -0,0 +1,22 @@
+package com.myframework.core.log;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.myframework.core.entity.BaseUserEntity;
+import com.myframework.core.filter.RequestFilter;
+
+/**
+ * Created by zw on 2017/7/24.
+ */
+public class ExtendConvert extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        String userInfo = "-1";
+        Long userId = (Long) RequestFilter.getSession().getAttribute(BaseUserEntity.USER_SESSION_ID);
+        if (userId != null) {
+            userInfo = userId.toString();
+        }
+        return userInfo;
+    }
+}
