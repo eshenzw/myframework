78086a1ed3407985bc4a0967398a08e81aee267f
赵伟 <zhaowei_ha@126.com>
2017-10-07 11:36:38 +0800

    增加 json处理器

---
+++ src/main/java/com/myframework/extend/JsonDeserializer/CustomJsonDateTimeDeserializer.java
@@ -0,0 +1,27 @@
+package com.myframework.extend.JsonDeserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by zw on 2017/10/7.
+ */
+public class CustomJsonDateTimeDeserializer extends JsonDeserializer {
+    @Override
+    public Date deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = jp.getText();
+        try {
+            return format.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
